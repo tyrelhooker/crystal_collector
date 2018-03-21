851667f99db03dfff299115811f2299d6c23e93d
Tyrel Hooker <tyrelhooker@gmail.com>
2018-03-20 22:05:24 -0400

    reset added, need to change individual character attr on reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,75 @@
-var crystalNumberArr = [];
-var crystalImagesArray= [
+var characterNumberArr = [];
+var characterImagesArray= [
   "assets/images/wonderwoman.png",
   "assets/images/wolverine.png",
   "assets/images/batman.png",
   "assets/images/spiderman.png"
 ];
-var randomNumber = Math.floor( Math.random() * 102) + 19;
-var wins = 0;
+var randomNumber;
 var losses = 0;
-var crystal = $(".crystal");
+var character = $(".character");
 var playerCounter = 0;
 
-$(".randomNumber").append(randomNumber);
-$(".userScore").append(playerCounter);
+
 
 function generateRan(){
-  var crystalRandomNumber;
+  var characterRandomNumber;
   for (i = 0; i < 4; i++) {
-    crystalRandomNumber = Math.floor( Math.random() * 12) + 1;
-    if (crystalNumberArr.indexOf(crystalRandomNumber) === -1){
-      crystalNumberArr.push(crystalRandomNumber);
+    characterRandomNumber = Math.floor( Math.random() * 12) + 1;
+    if (characterNumberArr.indexOf(characterRandomNumber) === -1){
+      characterNumberArr.push(characterRandomNumber);
     } else {
       i --; 
     }
   }
-  console.log(crystalNumberArr);
+  console.log(characterNumberArr);
 };
-generateRan();
-
-for (var i = 0; i < crystalNumberArr.length; i ++) {
-  var imageCrystal = $("<img>");
-  imageCrystal.addClass("crystalImage");
-  imageCrystal.attr("src", crystalImagesArray[i]);
-  imageCrystal.attr("data-crystalValue", crystalNumberArr[i]);
-  crystal.append(imageCrystal);
-}
-
-crystal.on("click", ".crystalImage", function() {
-  var crystalValue = ($(this).attr("data-crystalValue"));
-  console.log (crystalValue);
-  crystalValue = parseInt(crystalValue);
-  playerCounter += crystalValue;
-  $(".userScore").text(playerCounter);
-  console.log (crystalValue);
-  console.log (playerCounter);
-  winlossCounter();
-});
 
 function winlossCounter () {
   if (playerCounter === randomNumber) {
     console.log("You win!");
     wins++;
     $("#wins").append(wins);
+    resetGame();
   } else if (playerCounter > randomNumber) {
     console.log("You lose!");
     losses++;
     $("#losses").append(losses);
+    resetGame();
   }
 }
 
-function resetGame () {}
+function resetGame () {
+  randomNumber = Math.floor( Math.random() * 102) + 19;
+  var wins = 0;
+  playerCounter = 0;
+  $(".randomNumber").text(randomNumber);
+  console.log("reset" + randomNumber);
+  $(".userScore").text(playerCounter);
+  console.log("reset" + playerCounter);
+  characterNumberArr = [];
+  generateRan();
+  
+}
+
+// BEGIN GAME 
+resetGame();
+
+for (var i = 0; i < characterNumberArr.length; i ++) {
+  var imagecharacter = $("<img>");
+  imagecharacter.addClass("characterImage");
+  imagecharacter.attr("src", characterImagesArray[i]);
+  imagecharacter.attr("data-characterValue", characterNumberArr[i]);
+  character.append(imagecharacter);
+}
+
+character.on("click", ".characterImage", function() {
+  var characterValue = ($(this).attr("data-characterValue"));
+  characterValue = parseInt(characterValue);
+  playerCounter += characterValue;
+  $(".userScore").text(playerCounter);
+  console.log (characterValue);
+  console.log (playerCounter);
+  winlossCounter();
+});
+
